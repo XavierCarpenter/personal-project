76487dd722380cf7d7e18d22a81ae3248b442be0
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-03-01 23:07:05 -0600

    updated landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,34 +8,73 @@ import "./Landing.css";
 // import image2 from "../../images/landing2.jpeg";
 // import image3 from "../../images/landing3.jpeg";
 
-
-
 class Landing extends Component {
   constructor() {
     super();
-    this.state = {
-      businesses: []
-    };
   }
 
   render() {
- 
-    return <div>
-        <div className="landing_intro">
-          <h1>Wavvie</h1>
-          <a href={process.env.REACT_APP_LOGIN}>
-            <p>Login/Sign up</p>
-          </a>
-        </div>
-        <div className="sec2_container">
-          <h1>Find Top Businesses in your area</h1>
-        </div>
-        <div>
-       
+    return <div className="body">
+        <div className="container">
+          <div className="slider">
+            <div className="slide">
+              <div className="overlay">
+                <div className="landing-nav">
+                  <h1>Wavvie</h1>
+                  <a href={process.env.REACT_APP_LOGIN}>
+                    <p>Login</p>
+                  </a>
+                  <a href={process.env.REACT_APP_LOGIN}>
+                    <p>Sign up</p>
+                  </a>
+                </div>
+                <h2 id="subtitle">Quality Profesionals</h2>
+                <p id="subtext">
+                  With Wavvie it's easy to find quality individuals across
+                  many different professions
+                </p>
+              </div>
+            </div>
+            <div className="slide">
+              <div className="overlay">
+                <div className="landing-nav">
+                  <h1>Wavvie</h1>
+                  <a href={process.env.REACT_APP_LOGIN}>
+                    <p>Login</p>
+                  </a>
+                  <a href={process.env.REACT_APP_LOGIN}>
+                    <p>Sign up</p>
+                  </a>
+                </div>
+                <h2 id="subtitle">Instant Scheduling</h2>
+                <p id="subtext">
+                  No more waiting in line or missing appointments. Wavvie
+                  makes it simple and fast to Scheduling appointments.
+                </p>
+              </div>
+            </div>
+            <div className="slide">
+              <div className="overlay">
+                <div className="landing-nav">
+                  <h1>Wavvie</h1>
+                  <a href={process.env.REACT_APP_LOGIN}>
+                    <p>Login</p>
+                  </a>
+                  <a href={process.env.REACT_APP_LOGIN}>
+                    <p>Sign up</p>
+                  </a>
+                </div>
+                <h2 id="subtitle">Beautiful Portfolios</h2>
+                <p id="subtext">
+                  Here Models, Photographers, and much more post photos and
+                  videos of their best work.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>;
   }
 }
-
 
 export default Landing;
